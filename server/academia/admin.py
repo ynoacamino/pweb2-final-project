b9485d2e25e41e27bbc11295ef57e718b7fdaeeb
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2024-06-16 23:16:47 -0500

    register models in admin page

--- server/academia/admin.py
+++ server/academia/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Teacher)
+admin.site.register(User)
+admin.site.register(Curso)
+admin.site.register(Review)
+admin.site.register(Section)
+admin.site.register(Pdf)
+
