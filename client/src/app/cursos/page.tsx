01171ae846ebaeb78396e09d6cbfb831776f6215
SteveArms <acunoc@unsa.edu.pe>
2024-06-26 10:23:23 -0500

    Actualizando titulos de pagina utilizando metadata

--- client/src/app/cursos/page.tsx
+++ client/src/app/cursos/page.tsx
@@ -3,6 +3,12 @@ import { CoursesList } from '@/components/pages/courses/CoursesList';
 import Info from '@/components/pages/courses/Info';
 import Title from '@/components/pages/courses/Title';
 import Questions from '@/components/pages/courses/Questions';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: ' Cursos | Learning ',
+  description: 'Explora nuestra variedad de cursos diseñados para aprender y mejorar tus habilidades en programación y desarrollo web.',
+}
 
 export default function cursosPage() {
   return (
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,11 @@
 import Pillars from '@/components/pages/home/Pillars';
 import Title from '@/components/pages/home/Title';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: ' Home | Learning ',
+  description: 'Pagina Principal de mi aplicacion',
+}
 
 export default function Home() {
   return (
