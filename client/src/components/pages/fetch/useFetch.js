11e3b3b143b53fd45cd5cdbf5c883b7a8d820b79
Jorghee <jorgeehuarsaya@gmail.com>
2024-07-06 15:05:18 -0500

    Fúncion general que se usará para las peticiones GET

---
+++ client/src/components/pages/fetch/useFetch.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+export function useFetch(url) {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then(response => {
+        if (!response.ok)
+          throw new Error('Respuesta HTTP incorrecta');
+
+        return response.json();
+      })
+      .then(data => setCourses(data))
+      .catch(error => {
+        console.error('Hubo un error inesperado:', error);
+      });
+  }, []);
+
+  return { courses }
+}
