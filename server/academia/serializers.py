646deb90ee0c6276c84b10932481fb271d32c089
Jorghee <jorgeehuarsaya@gmail.com>
2024-07-07 08:51:27 -0500

    Establecemos las relaciones del curso con las secciones y profesor. Django crea automaticamentee la relación inversa.

--- server/academia/serializers.py
+++ server/academia/serializers.py
@@ -11,20 +11,23 @@ class UserSerializer(serializers.ModelSerializer):
     model = User
     fields = ['id', 'name', 'phone_number', 'email']
 
+class SectionSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Section
+    fields = ['id', 'curso', 'name', 'description', 'video_url']
+
 class CursoSerializer(serializers.ModelSerializer):
+  teacher = TeacherSerializer(read_only=True)
+  sections = SectionSerializer(many=True, read_only=True, source='section_set')
+
   class Meta:
     model = Curso
-    fields = ['id', 'name', 'description', 'image_url', 'teacher', 'users']
+    fields = ['id', 'name', 'description', 'image_url', 'teacher', 'users', 'sections']
 
 class ReviewSerializer(serializers.ModelSerializer):
   class Meta:
     model = Review
     fields = ['id', 'comment', 'user', 'curso']
-
-class SectionSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Section
-    fields = ['id', 'curso', 'name', 'description', 'video_url']
 
 class PdfSerializer(serializers.ModelSerializer):
   class Meta:
