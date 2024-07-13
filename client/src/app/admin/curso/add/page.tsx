1411a59655dcee180552be2a0bc893519f739a1a
Yenaro Joel Noa Camino <97652778+ynoacamino@users.noreply.github.com>
2024-07-13 18:47:55 -0500

    Dev steve arms avance (#13)
    * Avance
    
    * Cambios pagina add de Curso
    
    * Cambios pagina add de Section
    
    * update the html structure of add Courses
    
    * Upload to images only
    
    * update the html structure of add Sections
    
    ---------
    
    Co-authored-by: SteveArms <acunoc@unsa.edu.pe>

--- client/src/app/admin/curso/add/page.tsx
+++ client/src/app/admin/curso/add/page.tsx
@@ -2,15 +2,36 @@
 
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Upload from '@/components/icons/complements/Upload';
+import Back from '@/components/icons/complements/Back';
 
-export default function Page() {
+export default async function Page() {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
+
   const [image, setImage] = useState<File | null>(null);
+  const [preview, setPreview] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (image) {
+      const objectURL = URL.createObjectURL(image);
+      setPreview(objectURL);
+
+      return () => {
+        URL.revokeObjectURL(objectURL);
+      };
+    } else {
+      setPreview(null); // Limpiar la vista previa si no hay imagen
+    }
+  }, [image]);
+
+  const handleDelete = () => {
+    setImage(null);
+    setPreview(null);
+  }
 
   const router = useRouter();
-
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -36,49 +57,52 @@ export default function Page() {
   };
 
   return (
-    <div className="flex flex-col items-center gap-6 p-6 bg-gray-100 rounded-lg shadow-lg max-w-md mx-auto">
-      <h1 className="text-2xl font-bold mb-4 ">Agregar Curso</h1>
-      <form className="space-y-4" onSubmit={handleSubmit}>
-        <div>
-          <label className="block text-lg font-medium mb-2">Nombre del Curso:</label>
-          <input
-            type="text"
-            placeholder="Ingrese el nombre"
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-          />
-        </div>
-        <div>
-          <label className="block text-lg font-medium mb-2">Descripción:</label>
-          <textarea
-            placeholder="Ingresar Descripción"
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            onChange={(e) => setDescription(e.target.value)}
-            value={description}
-          />
-        </div>
-        <div>
-          <label className="block text-lg font-medium mb-2">Imagen:</label>
-          <input
-            type="file"
-            accept="image/*"
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            onChange={(e) => {
-              const file = e.target.files;
+    <div className="flex w-full md:px-60 md:py-20 px-6 my-10 md:my-2">
+      <form action="POST" className="flex w-full md:flex-row flex-col" onSubmit={handleSubmit}>
+        <a href="./" className='absolute justify-start mb-4'> <Back/> </a>
+          <div className='flex md:h-screen justify-center items-center flex-col w-full md:w-7/12 space-y-4 '>
+            <p className='md:text-4xl text-3xl font-bold text-primary'> ¡Bienvenido! </p>
+            <p className='md:text-3xl text-base font-bold '> Agrega el Curso </p>
+            <div className='flex flex-col w-full items-center justify-center space-y-2 p-4'>
+                <label className='text-primary font-bold md:text-xl text-base md:my-4 my-2'> Nombre: </label>
+                <input placeholder='Ingrese el nombre... ' className='md:text-base md:w-96 w-5/6 p-2 text-sm rounded-md bg-card shadow-lg' type="text" onChange={(e) => setName(e.target.value)} value={name}/>
+            </div>
+            <div className='flex flex-col w-full items-center justify-center space-y-2 p-4'>
+                <label className='text-primary font-bold md:text-xl text-base md:my-4 my-2'> Descripcion: </label>
+                <textarea placeholder='Ingrese la descripcion... ' className='md:text-sm text-xs md:w-96 w-5/6 p-4 rounded-md bg-card shadow-lg h-32' onChange={(e) => setDescription(e.target.value)} value={description}/>
+            </div>
+            <div className='flex justify-center mx-auto my-5'>
+              <Button onChange={handleDelete} className='md:p-6 p-2 font-bold md:text-base text-xs my-4'> Realizar Cambios </Button>
+            </div>
+          </div>
+          <div className='flex bg-primary w-full h-72 md:h-screen md:w-5/12 justify-center items-center flex-col'>
+            <div className="flex md:h-2/5 md:w-3/5 h-3/5 w-3/5 bg-card md:my-4 my-2 rounded-md">
+              {preview && (
+                <div className='relative'>
+                  <img src={preview} alt="Preview" className="object-contain p-8 mx-auto w-full h-full rounded-md"/>
+                  <Button onClick={handleDelete} className='md:text-sm text-xs md:left-6 left-2 absolute bottom-1 md:bottom-4 bg-red-600 hover:bg-red-400 px-1 md:py-2 md:px-4 rounded-md shadow-md'> Eliminar Imagen </Button>
+                </div>
+              )}
+            </div>
+            <span className='my-6'>
+            <label className="cursor-pointer md:text-base text-xs font-bold bg-card md:p-4 p-2 rounded-md flex items-center">
+                <Upload />  Subir Imagen
+                <input
+                style={{display:'none'}}
+                type="file"
+                name="image"
+                accept="image/*"
+                onChange={(e) => {
+                  const file = e.target.files;
+                  if (!file) {
+                    return;
+                  }
+                  setImage(file[0]);
+                }}/>
+              </label>
+            </span>
+          </div>
 
-              if (!file) {
-                return;
-              }
-              setImage(file[0]);
-            }}
-          />
-        </div>
-        <div>
-          <Button type="submit">
-            Agregar
-          </Button>
-        </div>
       </form>
     </div>
   );
--- client/src/app/admin/seccion/add/page.tsx
+++ client/src/app/admin/seccion/add/page.tsx
@@ -1,13 +1,32 @@
 'use client';
 
 import { Button } from "@/components/ui/button"
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useRouter} from 'next/navigation';
+import { useState, useEffect } from 'react';
+import Upload from "@/components/icons/complements/Upload";
+import Back from "@/components/icons/complements/Back";
 
 export default function PageAdd(){
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
     const [video, setVideo] = useState<File | null>(null);
+
+    const [preview, setPreview] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (video) {
+        const objectURL = URL.createObjectURL(video);
+        setPreview(objectURL);
+
+        return () => {
+            URL.revokeObjectURL(objectURL);
+        };
+        }
+    }, [video]);
+    const handleDelete = () => {
+      setVideo(null);
+      setPreview(null);
+    }
 
     const router = useRouter();
 
@@ -36,31 +55,52 @@ export default function PageAdd(){
     };
 
     return (
-        <div className="container mx-auto p-4">
-            <h1 className="text-2xl font-bold mb-4">Curso:</h1>
-            <h1 className="text-2xl font-bold mb-4">Agregar Sección</h1>
-            <form action="" method="POST" className="space-y-4" onSubmit={handleSubmit}>
-                <div>
-                    <label className="block text-lg font-medium mb-2">Nombre de la Sección:</label>
-                    <input 
-                        type="text" 
-                        placeholder="Ingrese el nombre" 
-                        className="w-full p-2 border rounded"
-                        onChange={(e) => setName(e.target.value)}
-                    />
+      <div className="flex w-full md:px-60 md:py-20 px-6 my-10 md:my-2">
+      <form action="POST" className="flex w-full md:flex-row flex-col" onSubmit={handleSubmit}>
+        <a href="./" className='absolute justify-start mb-4'> <Back/> </a>
+          <div className='flex md:h-screen justify-center items-center flex-col w-full md:w-7/12 space-y-4 '>
+            <p className='md:text-4xl text-3xl font-bold text-primary'> ¡Bienvenido! </p>
+            <p className='md:text-3xl text-base font-bold '> Agrega la Seccion </p>
+            <div className='flex flex-col w-full items-center justify-center space-y-2 p-4'>
+                <label className='text-primary font-bold md:text-xl text-base md:my-4 my-2'> Nombre: </label>
+                <input placeholder='Ingrese el nombre... ' className='md:text-base md:w-96 w-5/6 p-2 text-sm rounded-md bg-card shadow-lg' type="text" onChange={(e) => setName(e.target.value)} value={name}/>
+            </div>
+            <div className='flex flex-col w-full items-center justify-center space-y-2 p-4'>
+                <label className='text-primary font-bold md:text-xl text-base md:my-4 my-2'> Descripcion: </label>
+                <textarea placeholder='Ingrese la descripcion... ' className='md:text-sm text-xs md:w-96 w-5/6 p-4 rounded-md bg-card shadow-lg h-32' onChange={(e) => setDescription(e.target.value)} value={description}/>
+            </div>
+            <div className='flex justify-center mx-auto my-5'>
+              <Button onChange={handleDelete} className='md:p-6 p-2 font-bold md:text-base text-xs my-4'> Guardar Seccion </Button>
+            </div>
+          </div>
+          <div className='flex bg-primary w-full h-72 md:h-screen md:w-5/12 justify-center items-center flex-col'>
+            <div className="flex md:h-2/5 md:w-3/5 h-3/5 w-3/5 bg-card md:my-4 my-2 rounded-md">
+              {preview && (
+                <div className='relative'>
+                  <video src={preview} className="object-contain p-8 mx-auto w-full h-4/5 rounded-md" controls />
+                  <Button onClick={handleDelete} className='md:text-sm text-xs md:left-6 left-2 absolute bottom-1 md:bottom-4 bg-red-600 hover:bg-red-400 px-1 md:py-2 md:px-4 rounded-md shadow-md'> Eliminar Video </Button>
                 </div>
-                <div>
-                    <label className="block text-lg font-medium mb-2">Ingrese el video:</label>
-                    <input type="file"  className="w-full p-2 border rounded"/>
-                </div>
-                <div>
-                    <label className="block text-lg font-medium mb-2">Ingrese la Descripción:</label>
-                    <textarea placeholder="Ingresar Descripción" className="w-full p-2 border rounded " onChange={(e) => setDescription(e.target.value)}></textarea>
-                </div>
-                <div>
-                    <Button className="w-full p-2 font-bold rounded cursor-pointer " type="submit"> Subir Curso </Button>
-                </div>
-            </form>
-        </div>
+              )}
+            </div>
+            <span className='my-6'>
+            <label className="cursor-pointer md:text-base text-xs font-bold bg-card md:p-4 p-2 rounded-md flex items-center">
+                <Upload />  Subir Video
+                <input
+                style={{display:'none'}}
+                type="file"
+                name="image"
+                accept="image/*, video/*" 
+                onChange={(e) => {
+                  const file = e.target.files;
+                  if (!file) {
+                    return;
+                  }
+                  setVideo(file[0]);
+                }}/>
+              </label>
+            </span>
+          </div>
+        </form>
+      </div>
     );
 }
